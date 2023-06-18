a1c6365713642a684f67db7d9d485cfdb6f54b16
Purwa Darozatun <purwadarozatunakis@gmail.com>
2023-06-18 19:20:38 +0700

    update to snakecase

--- bin/helper/GenerateHelper.js
+++ bin/helper/GenerateHelper.js
@@ -31,7 +31,12 @@ const titleCase = (s) =>
         .map((x) => (x.charAt(0).toUpperCase() + x.slice(1)))
         .join("");
 
-const camelToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+const camelToSnakeCase = str =>
+  str &&
+  str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.toLowerCase())
+    .join('_');
 
 module.exports = {
     doGenerateSacaffond,
