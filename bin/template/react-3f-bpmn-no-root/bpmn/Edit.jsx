d6f143b2200cad60c5354bd30511d94aa224df8a
flashdin <flashdin007@gmail.com>
2023-06-19 13:41:04 +0700

    coba ganti scafond

---
+++ bin/template/react-3f-bpmn-no-root/bpmn/Edit.jsx
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from 'react';
+
+
+const Edit = () => {
+    
+    return  <>Ini Untuk Edit Process INstance bisa di edit di {{teamplatePath}}</>
+};
+
+export default Edit;
