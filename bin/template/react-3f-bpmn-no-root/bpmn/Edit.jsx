a221468514c3b172813e1aae984e1199d17b5300
flashdin <flashdin007@gmail.com>
2023-06-19 13:42:58 +0700

    Revert "coba ganti scafond"
    This reverts commit d6f143b2

--- bin/template/react-3f-bpmn-no-root/bpmn/Edit.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, useState} from 'react';
-
-
-const Edit = () => {
-    
-    return  <>Ini Untuk Edit Process INstance bisa di edit di {{teamplatePath}}</>
-};
-
-export default Edit;
--- bin/template/react-3f-bpmn-no-root/bpmn/Index.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, useState} from 'react';
-
-
-const Index = () => {
-    
-    return  <> Index Page</>
-};
-
-export default Index;
